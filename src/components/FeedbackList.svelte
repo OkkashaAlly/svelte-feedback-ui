4a83780b4b8b890484fcddbe2e009089c0815182
OkkashaAlly <steamnature006@gmail.com>
2023-11-17 07:53:36 +0300

    Getting feedbacks

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,9 @@
+<script>
+  import FeedbackItem from "./FeedbackItem.svelte";
+
+  export let feedbackList = [];
+</script>
+
+{#each feedbackList as feedback}
+  <FeedbackItem item={feedback} />
+{/each}
