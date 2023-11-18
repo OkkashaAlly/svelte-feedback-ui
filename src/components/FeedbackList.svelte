2b49d5c8a025ff988f8c07ccb739b595031819a0
OkkashaAlly <steamnature006@gmail.com>
2023-11-19 00:00:39 +0300

    in and out transitions added to feedback list

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,9 +1,12 @@
 <script>
+  import { fade, scale } from "svelte/transition";
   import FeedbackItem from "./FeedbackItem.svelte";
 
   export let feedbackList = [];
 </script>
 
-{#each feedbackList as feedback}
-  <FeedbackItem item={feedback} on:delete-feedback />
+{#each feedbackList as feedback (feedback.id)}
+  <div in:scale out:fade={{ duration: 500 }}>
+    <FeedbackItem item={feedback} on:delete-feedback />
+  </div>
 {/each}
